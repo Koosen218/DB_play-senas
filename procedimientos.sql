0b3516a9836eeadc7c3e7505cebdb9e79b111ded
WaxXaa <mfalejesus@criptext.com>
2024-07-07 18:29:13 -0500

    add: bloque para reconstruir todo

--- procedimientos.sql
+++ procedimientos.sql
@@ -4,12 +4,14 @@ CREATE OR REPLACE PROCEDURE crear_usuario(
   IN _nombre VARCHAR(50),
   IN _apellido VARCHAR(50),
   IN _email VARCHAR(50),
-  IN _password VARCHAR(50),
+  IN _contra VARCHAR(50),
   IN _foto_perfil VARCHAR(400),
-  IN _tipo VARCHAR(50),
+  IN _tipo VARCHAR(50)
 )
-  DECLARE
-  nombre VARCHAR(;
+DECLARE
+  INSERT INTO Tipo_Usuario (nombre, apellido, email, contra, _foto_perfil, tipo) VALUES (_nombre, _apellido, _email, _contra, _foto_perfil, _tipo);
+
+  
 BEGIN
 END//
 DELIMITER;
--- tablas.sql
+++ tablas.sql
@@ -3,10 +3,9 @@ show DATABASES;
 
 CREATE OR REPlACE DATABASE `juegos_senas`;
 USE `juegos_senas`
-
 SHOW TABLES;
 DROP TABLE IF EXISTS Etapas;
-CREATE TABLE Etapas (
+CREATE TABLE IF NOT EXISTS Etapas (
     id_etapa INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(500) NOT NULL,
@@ -15,7 +14,7 @@ CREATE TABLE Etapas (
 
 SHOW TABLES;
 DROP TABLE IF EXISTS Niveles;
-CREATE TABLE Niveles (
+CREATE TABLE IF NOT EXISTS Niveles (
     id_nivel INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(500) NOT NULL,
@@ -26,7 +25,7 @@ CREATE TABLE Niveles (
 
 SHOW TABLES;
 DROP TABLE IF EXISTS Lecciones;
-CREATE TABLE Lecciones (
+CREATE TABLE IF NOT EXISTS Lecciones (
     id_leccion INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(500) NOT NULL,
@@ -35,7 +34,7 @@ CREATE TABLE Lecciones (
 );
 SHOW TABLES;
 DROP TABLE IF EXISTS Niveles_Lecciones;
-CREATE TABLE Niveles_Lecciones (
+CREATE TABLE IF NOT EXISTS Niveles_Lecciones (
     id_nivele_leccion INT AUTO_INCREMENT,
     nivel INT,
     leccion INT,
@@ -46,13 +45,13 @@ CREATE TABLE Niveles_Lecciones (
 
 SHOW TABLES;
 DROP TABLE IF EXISTS Tipo_Preguntas;
-CREATE TABLE Tipo_Preguntas (
-  id_tipo INT  PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Tipo_Preguntas (
+  id_tipo INT PRIMARY KEY AUTO_INCREMENT,
   tipo VARCHAR(50) NOT NULL
 );
 SHOW TABLES;
 DROP TABLE IF EXISTS Preguntas;
-CREATE TABLE Preguntas (
+CREATE TABLE IF NOT EXISTS Preguntas (
     id_pregunta INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(500) NOT NULL,
@@ -66,7 +65,7 @@ CREATE TABLE Preguntas (
 
 SHOW TABLES;
 DROP TABLE IF EXISTS Respuestas;
-CREATE TABLE Respuestas (
+CREATE TABLE IF NOT EXISTS Respuestas (
     id_respuesta INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     imagenURL VARCHAR(255) NOT NULL,
@@ -75,7 +74,7 @@ CREATE TABLE Respuestas (
 );
 SHOW TABLES;
 DROP TABLE IF EXISTS Preguntas_Respuestas;
-CREATE TABLE Preguntas_Respuestas (
+CREATE TABLE IF NOT EXISTS Preguntas_Respuestas (
   id_pregunta_respuesta INT AUTO_INCREMENT,
   pregunta INT NOT NULL,
   respuesta INT NOT NULL,
@@ -84,18 +83,18 @@ CREATE TABLE Preguntas_Respuestas (
   CONSTRAINT Preguntas_Respuestas_RespuestasId_FK FOREIGN KEY (respuesta) REFERENCES Respuestas(id_respuesta)
 );
 SHOW TABLES;
-DROP TABLE IF EXISTS Orden_Respuesas;
-CREATE TABLE Orden_Respuesas (
+DROP TABLE IF EXISTS Orden_Respuestas;
+CREATE TABLE IF NOT EXISTS Orden_Respuestas (
   id_orden INT AUTO_INCREMENT,
   pregunta INT NOT NULL,
   imagenURL VARCHAR(255) NOT NULL,
   orden INT NOT NULL,
-  CONSTRAINT Orden_Respuesas_IdOrden_PK PRIMARY KEY (id_orden),
-  CONSTRAINT Orden_Respuesas_PreguntasId_FK FOREIGN KEY (pregunta) REFERENCES Preguntas(id_pregunta)
+  CONSTRAINT Orden_Respuestas_IdOrden_PK PRIMARY KEY (id_orden),
+  CONSTRAINT Orden_Respuestas_PreguntasId_FK FOREIGN KEY (pregunta) REFERENCES Preguntas(id_pregunta)
 );
 SHOW TABLES;
 DROP TABLE IF EXISTS Progreso;
-CREATE TABLE Progreso (
+CREATE TABLE IF NOT EXISTS Progreso (
     id_progreso INT AUTO_INCREMENT,
     etapa INT NOT NULL,
     nivel INT NOT NULL,
@@ -109,16 +108,14 @@ CREATE TABLE Progreso (
 SHOW TABLES;
 
 DROP TABLE IF EXISTS Tipo_Usuario;
-CREATE TABLE Tipo_Usuario (
+CREATE TABLE IF NOT EXISTS Tipo_Usuario (
     id_tipo INT,
     tipo VARCHAR(50) NOT NULL,
-    cod_tipo INT NOT NULL,
     CONSTRAINT Tipo_Usuario_IdTipo_PK PRIMARY KEY (id_tipo)
 );
 
 SHOW TABLES;
-DROP TABLE IF EXISTS Usuarios;
-CREATE OR REPLACE TABLE Usuarios (
+CREATE TABLE IF NOT EXISTS Usuarios (
   id_usuario INT NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
@@ -132,3 +129,46 @@ CREATE OR REPLACE TABLE Usuarios (
   CONSTRAINT Usuarios_ProgresoId_FK FOREIGN KEY (progreso) REFERENCES Progreso(id_progreso)
 );
 SHOW TABLES;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- borrar todo para levantar nuevamente
+-- Desactivar las verificaciones de claves foráneas
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Eliminar las tablas
+DROP TABLE IF EXISTS Usuarios;
+DROP TABLE IF EXISTS Tipo_Usuario;
+DROP TABLE IF EXISTS Progreso;
+DROP TABLE IF EXISTS Orden_Respuestas;
+DROP TABLE IF EXISTS Preguntas_Respuestas;
+DROP TABLE IF EXISTS Respuestas;
+DROP TABLE IF EXISTS Preguntas;
+DROP TABLE IF EXISTS Tipo_Preguntas;
+DROP TABLE IF EXISTS Niveles_Lecciones;
+DROP TABLE IF EXISTS Lecciones;
+DROP TABLE IF EXISTS Niveles;
+DROP TABLE IF EXISTS Etapas;
+
+-- Volver a activar las verificaciones de claves foráneas
+SET FOREIGN_KEY_CHECKS = 1;
